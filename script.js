ec98fc9757dc69905e9075096ba260fbd5b8a94f
MuhammadTahakhanjs <tahaoct2004@gmail.com>
2024-06-24 23:17:46 +0500

    typescript-firstclass-practice

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log('hello');
+console.log('hshsh');
+let user = 'Hello World';
+let rollNumber = 4102006;
+let isoff = true;
+console.log(user, rollNumber, isoff);
+function userName(userNam, msg) {
+    return `${userNam} ${msg}`;
+}
+userName('Taha', 'Kahn');
+console.log(userName('Tah', 'Kahn'));
+function message(greed, mark) {
+    console.log(greed + mark);
+}
+message('false ', 32);
---
+++ script.ts
@@ -0,0 +1,16 @@
+console.log('hello')
+console.log('hshsh')
+let user:string = 'Hello World';
+let rollNumber:number = 4102006;
+let isoff:boolean = true;
+console.log(user,rollNumber,isoff);
+function userName(userNam:string,msg:string):string{
+    return `${userNam} ${msg}`
+}
+userName('Taha','Kahn')
+console.log(userName('Tah','Kahn'))
+function message(greed:string,mark:number) :void{
+    console.log(greed  +  mark)
+
+}
+ message('false ', 32)
